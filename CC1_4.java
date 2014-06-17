3e44b4b9d6744eb40fd2d2fa6609e8ace64e0519
iwilbert <wilbert@Zhengpings-MacBook-Air.local>
2014-06-17 14:19:09 -0400

    CC1.4

---
+++ CC1_4.java
@@ -0,0 +1,41 @@
+
+public class CC1_4 {
+
+	public void replace(char[] line) {
+		int len;					// original length;
+		int cnt = 0;				// space count
+		for(len = 0; line[len] != '\0'; len++)
+		{
+			if(line[len] == ' ')
+				cnt++;
+		}
+		int new_len = len + cnt*2;	// length of new string
+		int k = new_len - 1;		// starts from the end of new string
+		for(int i = len - 1; i >= 0; i--)		// i starts from the end of original string
+		{
+			if(line[i] == ' ')
+			{
+				line[k] = '0';
+				line[k-1] = '2';
+				line[k-2] = '%';
+				k -= 2;
+			}
+			else
+				line[k] = line[i];
+			k--;
+		}
+    }
+	
+	public static void main(String[] args) {
+		char[] line = new char[10];
+		line[0] = 'A';
+		line[1] = ' ';
+		line[2] = 'B';
+		line[3] = ' ';
+		line[4] = 'C';
+		System.out.println(line);
+		new Problem4().replace(line);
+		System.out.println(line);
+	}
+	
+}
